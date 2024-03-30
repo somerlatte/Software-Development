3b0ff215eb40d3029aeed77e2bfa3cd522702a9b
Lorrayne Somerlatte <75647071+somerlatte@users.noreply.github.com>
2024-03-30 20:50:40 -0300

    Add files via upload

---
+++ VPL 4 - Decorador de Formas (5,0 pts)/OpacidadeDecorator.cpp
@@ -0,0 +1,18 @@
+#include "OpacidadeDecorator.hpp"
+
+std::string OpacidadeDecorator::desenha() {
+    if(this->_opacidade>=0&&this->_opacidade<=0.33){ 
+        return FormaDecorator::desenha() + "\n- opacidade: " + "baixa";}
+    if (this->_opacidade>=0.33&&this->_opacidade<=0.66){        
+        return FormaDecorator::desenha() + "\n- opacidade: " + "média";}
+    if(this->_opacidade>=0.66&&this->_opacidade<=1.0) {            
+        return FormaDecorator::desenha() + "\n- opacidade: " + "alta";}   
+    else{return FormaDecorator::desenha();} }
+
+void OpacidadeDecorator::setOpacidade(double opacidade) {
+  this->_opacidade = opacidade;
+}
+
+double OpacidadeDecorator::getOpacidade() {
+  return this->_opacidade;
+}
